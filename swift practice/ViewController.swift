feca6ccfdbd4b82f4437f11416a9684d6c77bfb1
Mostafa <dev.mostafatabasi@gmail.com>
2025-10-03 13:18:08 +0330

    Initial Commit

---
+++ swift practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift practice
+//
+//  Created by mohsen tabasi on 10/3/25.
+//  Copyright © 2025 mstf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
