fdbbee0f3e4cf7d151c6ec63ce2fc9730277763a
Mostafa <dev.mostafatabasi@gmail.com>
2025-10-11 15:09:25 +0330

    [Quiz App] Added the endless questions and check the answer

--- swift practice/quizz/QuizzViewController.swift
+++ swift practice/quizz/QuizzViewController.swift
@@ -16,10 +16,39 @@ class QuizzViewController: UIViewController {
     @IBOutlet weak var truebutton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quizQuestions = [
+        ["Four + Two is equal to six", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
     }
     
     @IBAction func onAnswerPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quizQuestions[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right")
+        } else {
+            print("Wrong")
+        }
+        
+        if questionNumber < quizQuestions.count - 1 {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = quizQuestions[questionNumber][0]
     }
 }
