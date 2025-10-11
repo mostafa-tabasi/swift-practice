15ce2ac6e1055d143989b8a76cb270caa3c63f7d
Mostafa <dev.mostafatabasi@gmail.com>
2025-10-11 15:52:45 +0330

    [Quiz App] Added question struct and used it

---
+++ swift practice/quizz/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  swift practice
+//
+//  Created by mohsen tabasi on 10/11/25.
+//  Copyright © 2025 mstf. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
