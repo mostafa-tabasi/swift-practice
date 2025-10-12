20d507411d8b06b64f58edb8de0e7a04da7d5d47
Mostafa <dev.mostafatabasi@gmail.com>
2025-10-12 19:48:06 +0330

    [BMI Calculator] Added the calculation logic

--- swift practice/bmi_calculator/controller/BMICalculatorViewController.swift
+++ swift practice/bmi_calculator/controller/BMICalculatorViewController.swift
@@ -10,15 +10,42 @@ import Foundation
 import UIKit
 
 class BMICalculatorViewController: UIViewController {
+    
+    var bmiValue = "0.0"
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad(){
         super.viewDidLoad()
     }
     
     @IBAction func onHeightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        let height =  String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
     }
     
     @IBAction func onWeightSliderChanged(_ sender: UISlider) {
-        print(sender.value)
+        let weight = String(Int(sender.value))
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func onCalculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        bmiValue = String(format: "%.1f", bmi)
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+        }
     }
 }
--- swift practice/bmi_calculator/controller/ResultViewController.swift
+++ swift practice/bmi_calculator/controller/ResultViewController.swift
@@ -10,7 +10,19 @@ import Foundation
 import UIKit
 
 class ResultViewController: UIViewController {
+    
+    var bmiValue : String?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
     override func viewDidLoad(){
         super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+    }
+    
+    @IBAction func onRecalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
