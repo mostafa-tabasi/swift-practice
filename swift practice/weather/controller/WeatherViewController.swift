c9da5e2c693fc52547b0b3aa8c2885be964a8fb4
Mostafa <dev.mostafatabasi@gmail.com>
2025-10-13 18:18:54 +0330

    [Weather App] Initiated the app

---
+++ swift practice/weather/controller/WeatherViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherController.swift
+//  swift practice
+//
+//  Created by mohsen tabasi on 10/13/25.
+//  Copyright © 2025 mstf. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
