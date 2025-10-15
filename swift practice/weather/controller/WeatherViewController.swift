34b3ae76730315c2f3c3343a97412341eef1cc96
Mostafa <dev.mostafatabasi@gmail.com>
2025-10-15 15:16:51 +0330

    [Weather App] Handled fetching the weather with location lat and long

--- swift practice/weather/controller/WeatherViewController.swift
+++ swift practice/weather/controller/WeatherViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
     
@@ -16,11 +17,21 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var searchTextField: UITextField!
     
     var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
         weatherManager.delegate = self
         searchTextField.delegate = self
+    }
+    
+    @IBAction func onLocationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
     }
 }
 
@@ -66,10 +77,29 @@ extension WeatherViewController : WeatherManagerDelegate {
         DispatchQueue.main.async {
             self.conditionImageView.image = UIImage(systemName: weather.conditionName)
             self.tempratureLabel.text = weather.temperatureString
+            self.cityLabel.text = weather.cityName
         }
     }
     
     func didFailWithError(_ error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - Section CLLocationManagerDelegate
+
+extension WeatherViewController : CLLocationManagerDelegate {
+        
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
 }
