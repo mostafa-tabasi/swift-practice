fd26be5375ccb7292ac2b03a73e668b129e83ab3
Mostafa <dev.mostafatabasi@gmail.com>
2025-10-28 17:42:00 +0330

    [Todoey App] Added select and deselect functionality to the items in the list

--- swift practice/todoey/controller/TodoeyViewController.swift
+++ swift practice/todoey/controller/TodoeyViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 class TodoeyViewController: UITableViewController {
 
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+        } else {
+            cell?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
